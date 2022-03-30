c257c14aab9b75d3cd04c58a8062d6a35e3e0fc9
Tayler Burke <taylerburke48@gmail.com>
2022-03-30 22:48:12 +0100

    Scroll to next section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 import { About } from './components/About/About';
+import { ChangeMe } from './components/Navbar/models';
 import { Navbar } from './components/Navbar/Navbar';
+import { Projects } from './components/Projects/Projects';
 
-function App() {
+const App = () => {
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const handleNextSection = (sectionRef: ChangeMe) => () => {
+    sectionRef.current.scrollIntoView();
+  }
   return (
     <div className="Portfolio">
       <Navbar />
-      <About />
+      <About innerRef={aboutRef} toNextSection={handleNextSection(projectsRef)} />
+      <Projects innerRef={projectsRef} />
     </div>
   );
 }
